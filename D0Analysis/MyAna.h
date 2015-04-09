ab803d743c3ba0a50aebfeed80d39477b48e0274
Elvire Bouvier <e.bouvier@ipnl.in2p3.fr>
2015-04-09 20:31:31 +0200

    still miss MyAna.cc and filelists...

---
+++ D0Analysis/MyAna.h
@@ -0,0 +1,72 @@
+#ifndef MYANA_h
+#define MYANA_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+#include <cmath>
+#include <iostream>
+#include <sstream>
+#include <set>
+#include "AnglesUtil.h"
+#include "RootTupleDefs.h"
+#include "TLorentzVector.h"
+class MyAna : public RootTupleDefs {
+
+public :
+  MyAna(TTree *_muonstree,TTree *_electronstree, TTree *_jetstree, TTree *_METtree, TTree *_verticestree, TTree *_eventstree, TTree *_HLTtree, TTree *_muonsloosetree, TTree *_electronsloosetree, TTree *_jpsitree, TTree *_mujetstree, TTree *_MCtree);
+  ~MyAna();
+  
+  void   Loop();
+  
+  void SetDebugMode(bool debug)                 { _debug    = debug;}   
+  void SetMCmode(bool isMC)                     { _isMC     = isMC;}
+  void SetTTbarmode(bool isTTbar)               { _isTTbar     = isTTbar;}
+  void SetSIGmode(bool isSIG)                   { _isSIG    = isSIG}
+  void SetRootName(const std::string& rootName) { _rootName = rootName;}
+  void SetDoSkim(bool doSkim)                   { _doSkim   = doSkim;}
+  void SetNevent(int nevent)                    { _nevt = nevent;}
+
+  TLorentzVector* GetP4(TClonesArray* _TClonesArray, int _i) {
+    return (TLorentzVector*) (*_TClonesArray)[_i];
+  }
+
+  double Pt(double _Px,double _Py) {
+    return std::sqrt(_Px*_Px+_Py*_Py);
+  }
+  
+  double Eta(double _Px,double _Py,double _Pz) {
+    if (_Pz <0) return (-1)*std::acosh(std::sqrt(_Px*_Px+_Py*_Py+_Pz*_Pz)/(std::sqrt(_Px*_Px+_Py*_Py)));
+    else return std::acosh(std::sqrt(_Px*_Px+_Py*_Py+_Pz*_Pz)/(std::sqrt(_Px*_Px+_Py*_Py)));
+  }
+
+  double Phi(double _Px,double _Py) {
+    return kinem::phi(_Py,_Px);
+  }
+  
+private:
+  
+  TFile* _newfile;
+
+  TTree* _newmuonstree;
+  TTree* _newelectronstree;
+  TTree* _newjetstree;
+  TTree* _newMETtree ;
+  TTree* _newverticestree;
+  TTree* _neweventstree;
+  TTree* _newHLTtree;
+  TTree* _newelectronsloosetree;
+  TTree* _newmuonsloosetree;
+  TTree* _newjpsitree;
+  TTree* _newmujetstree;
+  TTree* _newMCtree;
+  bool _debug;
+  bool _isMC;
+  bool _isTTbar;
+  bool _isSIG;
+  std::string _rootName;
+  bool _doSkim;
+  int _nevt;
+};
+
+#endif
---
+++ D0Analysis/run_ALL_MyAna.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#
+#==============================================================================#
+# Import Modules                                                               #
+#==============================================================================#
+
+import sys, os, string, shutil
+
+outRoot ="Plots_El_selection_date"
+#outRoot ="Plots_Mu_selection_date"
+exe = "./runMyAna"
+
+fileListDir = "filelists_15Apr01"
+
+mkdir="mkdir "+outRoot
+os.system(mkdir)
+
+copy = "cp MyAna.cc "+outRoot+"/"
+os.system(copy)
+
+filelist = os.popen("ls  "+fileListDir+"/*.list").readlines()
+
+for aFile in filelist:
+   
+    outRoo1 = outRoot+os.sep+string.split(string.strip(aFile),"/")[-1][:-5]+".root"
+    outLog1 = outRoot+os.sep+string.split(string.strip(aFile),"/")[-1][:-5]+".log"
+    
+    option  = " "
+#    option  = " -skim"
+    if aFile.count("SingleElectron") != 1 and aFile.count("SingleMu") != 1 and aFile.count("MuHad") != 1 and aFile.count("ElectronHad") != 1 :
+        option = option+" -mc"
+
+    if aFile.count("TTJets_FullLeptMGDecays") ==  1 or aFile.count("TTJets_SemiLeptMGDecays") == 1 :
+        option = option+" -sig"    
+        
+    if aFile.count("TTJets") == 1 or aFile.count("TTWJets") == 1 or aFile.count("TTWWJets") == 1 or aFile.count("TTZJets") ==1 :
+        option = option+" -ttbar"    
+        
+    cmd = exe+" -filelist "+string.strip(aFile)+" -out "+outRoo1+" "+option+"  >&  "+outLog1 
+    
+    print cmd
+    os.system(cmd)
+    
+sys.exit()
