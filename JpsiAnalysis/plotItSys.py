58c8e5ddd28189b85ecd2e1b4950a84b489e9e39
Elvire Bouvier <e.bouvier@ipnl.in2p3.fr>
2015-06-28 17:25:47 +0200

    consider systematics in plotIt

--- JpsiAnalysis/plotItSys.py
+++ JpsiAnalysis/plotItSys.py
@@ -5,7 +5,7 @@ import os, copy, datetime, pwd, re
 from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("-d", "--date", dest="date", type="string", default=False, help="date when MyAna was run")
-parser.add_option("-c", "--channel", dest="channel", type="string", default=False, help="electronic, muonic, all, or skim")
+parser.add_option("-c", "--channel", dest="channel", type="string", default=False, help="electronic, muonic, or all")
 parser.add_option("-v", "--version", dest="version", type="string", default=False, help="version of the day")
 parser.add_option("-t", "--decay", dest="decay", type="string", default=False, help="semi, di, or all leptonic")
 (options, args) = parser.parse_args()
@@ -20,50 +20,46 @@ if not os.path.isdir(dir):
 dir = os.path.join(dir, options.version)
 if not os.path.isdir(dir):
     parser.error("you must specify a valid version")
-if (not options.channel.lower().count("mu") and not options.channel.lower().count("el") and not options.channel.lower().count("all") and not options.channel.lower().count("skim")) or (not os.path.isdir(os.path.join(dir, "MyAnaMu")) and not os.path.isdir(os.path.join(dir, "MyAnaEl")) and not os.path.isdir(os.path.join(dir, "MyAnaAll")) and not os.path.isdir(os.path.join(dir, "MyAnaSkim"))):
+if (not options.channel.lower().count("mu") and not options.channel.lower().count("el") and not options.channel.lower().count("all") and not options.channel.lower().count("skim")) or (not os.path.isdir(os.path.join(dir, "MyAnaMu")) and not os.path.isdir(os.path.join(dir, "MyAnaEl")) and not os.path.isdir(os.path.join(dir, "MyAnaAll"))):
     parser.error("you must specify a valid channel")
 
 textChannel = " + Jets channel"
 
 if options.channel.lower().count("mu"):
-    outPlot = "PlotItMu"
+    outPlot = "PlotItSysMu"
     if options.decay.lower().count("semi"):
         textChannel = "#mu"+textChannel
     if options.decay.lower().count("di"):
         textChannel = "#mu#mu\/#mue"+textChannel
     if options.decay.lower().count("all"):
         textChannel = "#mu\/#mu#mu\/#mue"+textChannel
-    os.system("sed -e \"s/@root@/MyAnaMu/\" -e \"s/@lumi@/19705/\" -e \"s/@text@/%s/\" -e \"s/@dataFile@/MuHadASingleMuBCD.root/\" configPlotIt.template.yml > configPlotIt.yml" % (textChannel))
+    os.system("sed -e \"s/@root@/MyAnaMu/g\" -e \"s/@lumi@/19705/\" -e \"s/@text@/%s/\" -e \"s/@dataFile@/MuHadASingleMuBCD.root/\" configPlotItSys.template.yml > configPlotItSys.yml" % (textChannel))
 if options.channel.lower().count("el"):
-    outPlot = "PlotItEl"
+    outPlot = "PlotItSysEl"
     if options.decay.lower().count("semi"):
         textChannel = "e"+textChannel
     if options.decay.lower().count("di"):
         textChannel = "ee\/e#mu"+textChannel
     if options.decay.lower().count("all"):
         textChannel = "e\/ee\/e#mu"+textChannel
-    os.system("sed -e \"s/@root@/MyAnaEl/\" -e \"s/@lumi@/19690/\" -e \"s/@text@/%s/\" -e \"s/@dataFile@/ElectronHadASingleElectronBCD.root/\" configPlotIt.template.yml > configPlotIt.yml" % (textChannel))
+    os.system("sed -e \"s/@root@/MyAnaEl/g\" -e \"s/@lumi@/19690/\" -e \"s/@text@/%s/\" -e \"s/@dataFile@/ElectronHadASingleElectronBCD.root/\" configPlotItSys.template.yml > configPlotItSys.yml" % (textChannel))
 if options.channel.lower().count("all"):
-    outPlot = "PlotItAll"
+    outPlot = "PlotItSysAll"
     if options.decay.lower().count("semi"):
         textChannel = "e\/#mu"+textChannel
     if options.decay.lower().count("di"):
         textChannel = "ee\/#mu#mu\/e#mu"+textChannel
     if options.decay.lower().count("all"):
         textChannel = "e\/#mu\/ee\/#mu#mu\/e#mu"+textChannel
-    os.system("sed -e \"s/@text@/%s/\" configPlotItAll.template.yml > configPlotIt.yml" % (textChannel))
-if options.channel.lower().count("skim"):
-    outPlot = "PlotItSkim"
-    textChannel = "Skim"
-    os.system("sed -e \"s/@text@/%s/\" configPlotItSkim.template.yml > configPlotIt.yml" % (textChannel))
+    os.system("sed -e \"s/@text@/%s/\" configPlotItSysAll.template.yml > configPlotItSys.yml" % (textChannel))
 
 print "/!\\ you should not have sourced cmsenv"
 
 os.chdir(dir)
 if not os.path.isdir(outPlot):
     os.mkdir(outPlot)
-cmd = "../../../../plotIt/plotIt -o " + outPlot + " ../../configPlotIt.yml >& " + outPlot + "/NEvents.log &"
+cmd = "../../../../plotIt/plotIt -o " + outPlot + " ../../configPlotItSys.yml >& " + outPlot + "/NEvents.log "
 os.system(cmd)
 
 os.chdir("../../")
-os.system("rm -rf configPlotIt.yml")
+os.system("rm -rf configPlotItSys.yml")
